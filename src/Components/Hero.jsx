9d24bc6e5ea7f8b5912f02ce2668344eee9d86ae
surbhi6089 <66399968+surbhi6089@users.noreply.github.com>
2023-08-09 17:05:47 +0545

    changes for merge conflict test: DEV

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <h1>Online Experiences</h1>
         <p>
           Join unique interactive activities led by one-of-a-kind hosts—all
-          without leaving home.
+          without leaving home. New changesfor merge conflict test: DEV
         </p>
       </div>
     </div>
